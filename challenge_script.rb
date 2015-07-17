b2093b662b5665e3e301527932a0b3c618080042
Charlie Gaines <beaugaines@gmail.com>
2015-07-17 10:27:45 +0200

    refactoring

--- challenge_script.rb
+++ challenge_script.rb
@@ -4,24 +4,21 @@ require_relative 'meats'
 
 def meat_slideshow
   meats_o_the_day = MEATS.split('&')
-  puts meats_o_the_day.cycle(99) { |meat| system 'clear'; puts meat; sleep 4 }
+  puts meats_o_the_day.cycle(99) { |meat| system 'clear'; puts meat; sleep 3 }
 end
 
 uri = URI('http://aerial-valor-93012.appspot.com/challenge')
-challenge = JSON.parse(Net::HTTP.get(uri)) rescue "that ain't json"
+json_response = JSON.parse(Net::HTTP.get(uri)) rescue "that ain't json"
 
-token = challenge['token']
-sum = challenge['values'].inject(:+)
+token = json_response['token']
+sum = json_response['values'].inject(:+)
 
 success_uri = URI("http://aerial-valor-93012.appspot.com/challenge/#{token}/#{sum}")
-
-success_response = Net::HTTP.get(success_uri)
-
-secret_code = JSON.parse(success_response)['answer']
+secret_code = JSON.parse(Net::HTTP.get(success_uri))['answer']
 
 system 'clear'
 
 puts "#{"\n" * 10}#{secret_code.center(140)} :)"
-sleep 4
+sleep 3
 meat_slideshow
 
