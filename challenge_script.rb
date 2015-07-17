e6038ea883443486543160c2edd04f6e9ab8d1af
Charlie Gaines <beaugaines@gmail.com>
2015-07-17 10:34:22 +0200

    Revert "refactoring"
    This reverts commit b2093b662b5665e3e301527932a0b3c618080042.

--- challenge_script.rb
+++ challenge_script.rb
@@ -4,21 +4,24 @@ require_relative 'meats'
 
 def meat_slideshow
   meats_o_the_day = MEATS.split('&')
-  puts meats_o_the_day.cycle(99) { |meat| system 'clear'; puts meat; sleep 3 }
+  puts meats_o_the_day.cycle(99) { |meat| system 'clear'; puts meat; sleep 4 }
 end
 
 uri = URI('http://aerial-valor-93012.appspot.com/challenge')
-json_response = JSON.parse(Net::HTTP.get(uri)) rescue "that ain't json"
+challenge = JSON.parse(Net::HTTP.get(uri)) rescue "that ain't json"
 
-token = json_response['token']
-sum = json_response['values'].inject(:+)
+token = challenge['token']
+sum = challenge['values'].inject(:+)
 
 success_uri = URI("http://aerial-valor-93012.appspot.com/challenge/#{token}/#{sum}")
-secret_code = JSON.parse(Net::HTTP.get(success_uri))['answer']
+
+success_response = Net::HTTP.get(success_uri)
+
+secret_code = JSON.parse(success_response)['answer']
 
 system 'clear'
 
 puts "#{"\n" * 10}#{secret_code.center(140)} :)"
-sleep 3
+sleep 4
 meat_slideshow
 
